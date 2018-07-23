62b246d61062291119c2ad64031c06bfe7a3ec18
Vani Vismitha <vanivismitha@gmail.com>
2018-07-23 15:30:02 +0530

    Update HomePageController.java
    Removed from return statement new ModelAndView("home") fro line 24 for testing purpose.

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -21,6 +21,6 @@ public class HomePageController {
 	// Clicking the "home" text loads the main page
     @RequestMapping("/home")
     public ModelAndView home() {
-        return new ModelAndView("home");
+        return ;
     }
 }
